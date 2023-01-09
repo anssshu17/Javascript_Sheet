0d2c5f5ace91c2a01286c861a2f187a40671b14b
anssshu17 <rajanshu1703@gmail.com>
2023-01-09 23:52:15 +0530

    day2

---
+++ Day02/Easy/answers.js
@@ -0,0 +1,34 @@
+/*1.*/ let challenge ='30 days of javascript'
+/*2.*/ console.log(challenge)
+/*3.*/ console.log(challenge.length)
+/*4.*/ console.log(challenge.toUpperCase())
+/*5.*/ console.log(challenge.toLowerCase())
+/*6.*/ console.log(challenge.substr(0,2))
+/*7.*/ console.log(challenge.substring(2,21))
+/*8.*/ console.log(challenge.includes("script"))
+/*9.*/ console.log(challenge.split(' '))
+/*10.*/ console.log(challenge.split(' '))
+console.log(challenge.split(''))
+/*11.*/ let company = ('Facebook, Google, Microsoft, Apple, IBM, Amazon')
+console.log(company.split(', '))
+/*12.*/ console.log(challenge.replace("javascript", "python"))
+/*13.*/ console.log(challenge.charAt(15))
+/*14.*/ console.log(challenge.charCodeAt("j"))
+/*15.*/ console.log(challenge.indexOf("a"))
+/*16.*/ console.log(challenge.lastIndexOf("a"))
+/*17.*/ let sentence = "you cannot end a sentence with because because because is a conjuction"
+console.log(sentence.indexOf("because"))
+/*18.*/ console.log(sentence.lastIndexOf("because"))
+/*19.*/ console.log(sentence.search("because"))
+/*20.*/ console.log(challenge.trim(' '))
+/*21.*/ console.log(challenge.startsWith("30"))
+/*22.*/ console.log(challenge.endsWith("script"))
+/*23.*/ console.log(challenge.match("a"))
+/*24.*/ let merge = "30 days of"
+console.log(merge.concat("javascript"))
+/*25.*/ let name = "30 days of javascript"
+console.log(name.repeat(2))
+
+
+
+
